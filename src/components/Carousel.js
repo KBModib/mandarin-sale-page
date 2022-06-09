5e0e428f5e19c56e02b227a71bb651fc234cfa5f
KBModib <kmodibedi@outlook.com>
2022-06-09 17:14:13 +0200

    update

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,5 @@
 import React from 'react'
-//import mandarin from './images/mandarin.jpg'
 import Carousel from 'react-bootstrap/Carousel'
-import Button from 'react-bootstrap/Button'
 
 
 export default function CarouselSlider() {
@@ -9,69 +7,73 @@ export default function CarouselSlider() {
     <Carousel fade class="carousel">
          <Carousel.Item>
             <img
-                className="d-block w-100 "
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                className="d-block w-100 carousel-img"
+                src=""
                 alt="1 slide"
             />
-            <Carousel.Caption>
+            <Carousel.Caption class="carousel-text">
                 <h1>Useful Delicious Mandarins</h1>
                 <p>lorem ipsum dolor sit amet, nto nton, should have used a random text generator because isLatin angisazi.</p>
                 <h2>$1.65</h2>
-                <Button variant="outline-secondary" class='add-btn'>Add to cart</Button>{' '}
-                <h1>001</h1>
+                <button type="button" class='add-btn'>Add to cart</button>
         </Carousel.Caption>
+        <h1 class="item-number">001</h1>
         </Carousel.Item>
         <Carousel.Item>
             <img
-                className="d-block w-100 "
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                className="d-block w-100 carousel-img"
+                src=""
                 alt="2 slide"
             />
-            <Carousel.Caption>
+            <Carousel.Caption class="carousel-text">
                 <h1>Useful Delicious Mandarins</h1>
                 <p>lorem ipsum dolor sit amet, nto nton, should have used a random text generator because isLatin angisazi.</p>
                 <h2>$1.55</h2>
-                <h1>002</h1>
+                <button type="button" class='add-btn'>Add to cart</button>
             </Carousel.Caption>
+            <h1 class="item-number">002</h1>
         </Carousel.Item>
         <Carousel.Item>
             <img
-                className="d-block w-100 "
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                className="d-block w-100 carousel-img"
+                src=""
                 alt="Third slide"
             />
-            <Carousel.Caption>
+            <Carousel.Caption class="carousel-text">
                 <h1>Useful Delicious Mandarins</h1>
                 <p>lorem ipsum dolor sit amet, nto nton, should have used a random text generator because isLatin angisazi.</p>
                 <h2>$1.60</h2>
-                <h1>003</h1>
+                <button type="button" class='add-btn'>Add to cart</button>
             </Carousel.Caption>
+            <h1 class="item-number">003</h1>
         </Carousel.Item>
         <Carousel.Item>
             <img
-                className="d-block w-100 "
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                className="d-block w-100 carousel-img"
+                src=""
                 alt="4 slide"
             />
-            <Carousel.Caption>
+            <Carousel.Caption class="carousel-text">
                 <h1>Useful Delicious Mandarins</h1>
                 <p>lorem ipsum dolor sit amet, nto nton, should have used a random text generator because isLatin angisazi.</p>
                 <h2>$1.95</h2>
-                <h1>004</h1>
+                <button type="button" class='add-btn'>Add to cart</button>
             </Carousel.Caption>
+            <h1 class="item-number">004</h1>
         </Carousel.Item>    
         <Carousel.Item>
             <img
-                className="d-block w-100 "
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                className="d-block w-100 carousel-img"
+                src=""
                 alt="5 slide"
             />
-            <Carousel.Caption>
+            <Carousel.Caption class="carousel-text">
                 <h1>Useful Delicious Mandarins</h1>
                 <p>lorem ipsum dolor sit amet, nto nton, should have used a random text generator because isLatin angisazi.</p>
                 <h2>$1.50</h2>
-                <h1>005</h1>
+                <button type="button" class='add-btn'>Add to cart</button>
             </Carousel.Caption>
+            <h1 class="item-number">005</h1>
         </Carousel.Item>
     </Carousel>
 
